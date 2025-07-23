95581b2d4819f012a4ec7107f5efa6c563bce82f
Naveen Pandey <pandeynaveencsjmu2020@gmail.com>
2025-07-23 08:04:48 +0530

    commit after fixing Product page scroll issue and Hero Section background break issue

--- frontend/src/components/Landing_Page/Product_Section/Products.jsx
+++ frontend/src/components/Landing_Page/Product_Section/Products.jsx
@@ -11,12 +11,12 @@ import AppCard from "./AppCard"; // Importing the AppCard component
 // Component to display app cards
 const Products = () => {
   return (
-    <div className="max-h-[100vh] overflow-y-scroll">
-      <div className="text-black font-[Poppins] text-[32.607px] font-medium leading-none flex justify-center ml-10 mb-4">
+    <div className="min-h-screen max-h-[100vh] overflow-y-auto py-24 px-6 md:px-12">
+      <div className="text-black font-[Poppins] text-[32.607px] font-medium leading-none flex justify-center ml-10 mb-4 mt-[-80px]">
         Discover Our Complete Suite of Digital Tools
       </div>
 
-      <div className="flex flex-wrap justify-center mx-[100px]">
+      <div className="flex flex-wrap justify-center">
         {APPS.map((app, index) => (
           <div className="flex justify-center w-full sm:w-1/2 lg:w-1/3">
           <AppCard
--- frontend/src/utils/constants.jsx
+++ frontend/src/utils/constants.jsx
@@ -1,8 +1,8 @@
 export const CLIENT_ID = "733145859090-a841efgntmtqmpqn8u08oiulimf0leeu.apps.googleusercontent.com";
-// export const HOST = "http://localhost:4000"
-// export const FRONTENDHOST = "http://localhost:5173"
-export const HOST = ""
-export const FRONTENDHOST = "https://interact.ceoitbox.com"
+export const HOST = "http://localhost:4000"
+export const FRONTENDHOST = "http://localhost:5173"
+// export const HOST = ""
+// export const FRONTENDHOST = "https://interact.ceoitbox.com"
 import PeopleImage from "../assets/images/People_Card_Image.png";
 import VideoImage from "../assets/images/Video_Card_Image.png";
 import PhotoImage from "../assets/images/PhotoGallery_Card_Image.png";
